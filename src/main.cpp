066b8368683582253043a15f50eee43d4a8f5a8b
Jarrod Philips <4137235+jcphilips@users.noreply.github.com>
2020-03-16 22:07:40 +0530

    Update board to Uno

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <Arduino.h>
+#include <Servo.h>
 
+Servo claw;
 void setup() {
-  // put your setup code here, to run once:
+  claw.attach(11);
+  delay(1000);
+  claw.read();
 }
 
 void loop() {
